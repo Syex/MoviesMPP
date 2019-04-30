3343dfea577e7a6f3b6b02d1204b82ec06dce840
Tom Seifert <syex@seifertion.de>
2019-04-30 14:29:21 +0200

    Remove commented sample JSON from entity file

--- app/src/commonMain/kotlin/de/moviesmpp/data/entity/MovieEntity.kt
+++ app/src/commonMain/kotlin/de/moviesmpp/data/entity/MovieEntity.kt
@@ -20,24 +20,3 @@ data class MovieEntity(
     val overview: String,
     @SerialName("poster_path") val posterPath: String
 )
-
-/**
-"popularity": 437.513,
-"id": 299534,
-"video": false,
-"vote_count": 73,
-"vote_average": 7.4,
-"title": "Avengers: Endgame",
-"release_date": "2019-04-26",
-"original_language": "en",
-"original_title": "Avengers: Endgame",
-"genre_ids": [
-12,
-878,
-28
-],
-"backdrop_path": "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
-"adult": false,
-"overview": "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
-"poster_path": "/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
- */
